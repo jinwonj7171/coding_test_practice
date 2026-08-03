141e10df070e45b61a662b99429e9ce1344f9e72
JinwonJeong <ohya7171@naver.com>
2026-08-03 14:59:25 +0900

    JW commit 1

---
+++ src/coding_test_problem/SWEA1961.java
@@ -0,0 +1,13 @@
+package coding_test_problem;
+
+import java.io.*;
+
+public class SWEA1961 {
+
+	public static void main(String[] args)throws Exception {
+		BufferedReader br = new BufferedReader(new FileReader("input/swea_1961.txt"));
+		System.out.println(br.readLine());
+
+	}
+
+}
